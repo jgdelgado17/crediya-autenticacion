f2ab6e0598ed14a25eb842ff4c51817646fe15b3
Gabriel delgado <jegadele@hotmail.com>
2025-08-24 17:33:19 -0500

    refactor(role): RoleAdapter extends ReactiveAdapterOperations and RoleRepositoryCrud extends ReactiveQueryByExampleExecutor

--- infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/crediya/autenticacion/r2dbc/modules/role/adapter/RoleAdapter.java
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/crediya/autenticacion/r2dbc/modules/role/adapter/RoleAdapter.java
@@ -2,28 +2,31 @@ package co.com.crediya.autenticacion.r2dbc.modules.role.adapter;
 
 import co.com.crediya.autenticacion.model.role.Role;
 import co.com.crediya.autenticacion.model.role.gateways.RoleRepository;
-import co.com.crediya.autenticacion.r2dbc.modules.role.mapper.RoleMapper;
+import co.com.crediya.autenticacion.r2dbc.helper.ReactiveAdapterOperations;
+import co.com.crediya.autenticacion.r2dbc.modules.role.data.RoleEntity;
 import co.com.crediya.autenticacion.r2dbc.modules.role.repository.RoleRepositoryCrud;
-import lombok.RequiredArgsConstructor;
+import org.reactivecommons.utils.ObjectMapper;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 @Service
-@RequiredArgsConstructor
-public class RoleAdapter implements RoleRepository {
+public class RoleAdapter
+        extends ReactiveAdapterOperations<Role, RoleEntity, Integer, RoleRepositoryCrud>
+        implements RoleRepository {
 
-    private final RoleRepositoryCrud repository;
-    private final RoleMapper mapper;
+    public RoleAdapter(RoleRepositoryCrud repository, ObjectMapper mapper) {
+        super(repository, mapper, d -> mapper.map(d, Role.class));
+        this.repository = repository;
+    }
 
     @Override
     public Mono<Role> save(Role role) {
-        return repository.save(mapper.toEntity(role))
-                .map(mapper::toDomain);
+        return super.save(role);
     }
 
     @Override
     public Mono<Role> findByName(String name) {
         return repository.findByName(name)
-                .map(mapper::toDomain);
+                .map(super::toEntity);
     }
 }
--- infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/crediya/autenticacion/r2dbc/modules/role/repository/RoleRepositoryCrud.java
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/crediya/autenticacion/r2dbc/modules/role/repository/RoleRepositoryCrud.java
@@ -1,9 +1,10 @@
 package co.com.crediya.autenticacion.r2dbc.modules.role.repository;
 
 import co.com.crediya.autenticacion.r2dbc.modules.role.data.RoleEntity;
+import org.springframework.data.repository.query.ReactiveQueryByExampleExecutor;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Mono;
 
-public interface RoleRepositoryCrud extends ReactiveCrudRepository<RoleEntity, Integer> {
+public interface RoleRepositoryCrud extends ReactiveCrudRepository<RoleEntity, Integer>, ReactiveQueryByExampleExecutor<RoleEntity> {
     Mono<RoleEntity> findByName(String name);
 }
