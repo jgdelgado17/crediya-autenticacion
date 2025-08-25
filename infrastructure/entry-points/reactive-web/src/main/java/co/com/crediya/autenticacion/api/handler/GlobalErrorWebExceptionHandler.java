ccfdb790eaad9dfc271c188e7a8d5bc00aa2c145
jgdelgado17 <jegadele@hotmail.com>
2025-08-25 10:53:35 -0500

    feat(GlobalErrorWebExceptionHandler): add exception handler

---
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/autenticacion/api/handler/GlobalErrorWebExceptionHandler.java
@@ -0,0 +1,62 @@
+package co.com.crediya.autenticacion.api.handler;
+
+import org.springframework.boot.autoconfigure.web.WebProperties;
+import org.springframework.boot.autoconfigure.web.reactive.error.AbstractErrorWebExceptionHandler;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.reactive.error.ErrorAttributes;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.*;
+import reactor.core.publisher.Mono;
+import java.util.Map;
+
+@Component
+@Order(-2)
+public class GlobalErrorWebExceptionHandler extends AbstractErrorWebExceptionHandler {
+
+    public GlobalErrorWebExceptionHandler(ErrorAttributes errorAttributes,
+                                          WebProperties webProperties, // <--- Se inyecta la clase completa
+                                          ApplicationContext applicationContext,
+                                          ServerCodecConfigurer configurer) {
+        super(errorAttributes, webProperties.getResources(), applicationContext); // <--- Se accede a getResources()
+        this.setMessageWriters(configurer.getWriters());
+        this.setMessageReaders(configurer.getReaders());
+    }
+
+    @Override
+    protected RouterFunction<ServerResponse> getRoutingFunction(ErrorAttributes errorAttributes) {
+        return RouterFunctions.route(RequestPredicates.all(), this::renderErrorResponse);
+    }
+
+    private Mono<ServerResponse> renderErrorResponse(ServerRequest request) {
+        Throwable error = getError(request);
+        HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+        String errorMessage = "Internal Server Error";
+
+        if (error instanceof IllegalArgumentException) {
+            httpStatus = HttpStatus.BAD_REQUEST;
+            errorMessage = error.getMessage();
+        } /*else if (error instanceof DataAccessResourceFailureException) {
+            httpStatus = HttpStatus.SERVICE_UNAVAILABLE;
+            errorMessage = "Database service is unavailable. Please try again later.";
+        } */
+
+        Map<String, Object> errorAttributes = getErrorAttributes(request, ErrorAttributeOptions.defaults());
+        //Map<String, Object> errorAttributes = getErrorAttributes(request, ErrorAttributeOptions.of(Include.STACK_TRACE));
+        errorAttributes.put("status", httpStatus.value());
+        errorAttributes.put("error", httpStatus.getReasonPhrase());
+        errorAttributes.put("message", errorMessage);
+        errorAttributes.remove("exception");
+        errorAttributes.remove("path");
+        errorAttributes.remove("requestId");
+
+        return ServerResponse.status(httpStatus)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(errorAttributes));
+    }
+}
