46e8804767f4fba6f548f4aa42c36f09563ec94e
Gabriel delgado <jegadele@hotmail.com>
2025-08-24 20:56:20 -0500

    feat(RoleController): add reactive-web module

---
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/autenticacion/api/controllers/RoleController.java
@@ -0,0 +1,27 @@
+package co.com.crediya.autenticacion.api.controllers;
+
+import co.com.crediya.autenticacion.model.role.Role;
+import co.com.crediya.autenticacion.usecase.role.RoleUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/v1/role")
+@RequiredArgsConstructor
+public class RoleController {
+    private final RoleUseCase roleUseCase;
+
+    @PostMapping
+    public Mono<ResponseEntity<Role>> createRole(@RequestBody Role role) {
+        System.out.println("Role : " + role.getNames());
+        return roleUseCase.createRole(role)
+                .map(ResponseEntity::ok)
+                .onErrorResume(Mono::error)
+                .switchIfEmpty(Mono.just(ResponseEntity.badRequest().build()));
+    }
+}
