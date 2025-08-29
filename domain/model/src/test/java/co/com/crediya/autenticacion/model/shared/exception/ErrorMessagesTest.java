8da815a7e5f680a6e8f6b88cb9b671de15a9c526
jgdelgado17 <jegadele@hotmail.com>
2025-08-29 11:50:58 -0500

    feat(modelTest): add unit test EnumUtilsTest and ErrorMessagesTest

---
+++ domain/model/src/test/java/co/com/crediya/autenticacion/model/shared/exception/ErrorMessagesTest.java
@@ -0,0 +1,45 @@
+package co.com.crediya.autenticacion.model.shared.exception;
+
+import org.junit.jupiter.api.Test;
+
+class ErrorMessagesTest {
+
+    @Test
+    void shouldReturnNotFoundMessage() {
+        // Arrange
+        Class<?> clazz = String.class;
+        Object field = "testField";
+
+        // Act
+        String result = ErrorMessages.notFoundMessage(clazz, field);
+
+        // Assert
+        assert result.equals("String not found : testField");
+    }
+
+    @Test
+    void shouldReturnRequiredFieldMessage() {
+        // Arrange
+        String fieldName = "username";
+
+        // Act
+        String result = ErrorMessages.requiredField(fieldName);
+
+        // Assert
+        assert result.equals("username is required");
+    }
+
+    @Test
+    void shouldReturnInvalidEnumValueMessage() {
+        // Arrange
+        String label = "RoleEnum";
+        String value = "INVALID";
+
+        // Act
+        String result = ErrorMessages.invalidEnumValue(label, value);
+
+        // Assert
+        assert result.equals("Invalid RoleEnum: INVALID");
+    }
+
+}
