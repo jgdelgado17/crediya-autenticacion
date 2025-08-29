0a5d7c6b732a499b85459f91f196a5a131966127
jgdelgado17 <jegadele@hotmail.com>
2025-08-29 18:40:58 -0500

    refactor(ConfigTest): change test GET to test POST

--- infrastructure/entry-points/reactive-web/src/test/java/co/com/crediya/autenticacion/api/config/ConfigTest.java
+++ infrastructure/entry-points/reactive-web/src/test/java/co/com/crediya/autenticacion/api/config/ConfigTest.java
@@ -2,12 +2,26 @@ package co.com.crediya.autenticacion.api.config;
 
 import co.com.crediya.autenticacion.api.Handler;
 import co.com.crediya.autenticacion.api.RouterRest;
+import co.com.crediya.autenticacion.api.mapper.RoleDataMapper;
+import co.com.crediya.autenticacion.api.mapper.UserDataMapper;
+import co.com.crediya.autenticacion.usecase.role.RoleUseCase;
+import co.com.crediya.autenticacion.usecase.user.UserUseCase;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.validation.Validator;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
 
 @ContextConfiguration(classes = {RouterRest.class, Handler.class})
 @WebFluxTest
@@ -17,12 +31,35 @@ class ConfigTest {
     @Autowired
     private WebTestClient webTestClient;
 
+    @MockBean
+    private Handler handler;
+
+    @MockBean
+    private RouterRest routerRest;
+
     @Test
     void corsConfigurationShouldAllowOrigins() {
-        webTestClient.get()
+        // Given
+        when(routerRest.routerFunction(any())).thenReturn(
+                RouterFunctions.route(
+                        POST("/api/usecase/path"),
+                        request -> ServerResponse.ok()
+                                .header("Content-Security-Policy", "default-src 'self'; frame-ancestors 'self'; form-action 'self'")
+                                .header("Strict-Transport-Security", "max-age=31536000;")
+                                .header("X-Content-Type-Options", "nosniff")
+                                .header("Server", "")
+                                .header("Cache-Control", "no-store")
+                                .header("Pragma", "no-cache")
+                                .header("Referrer-Policy", "strict-origin-when-cross-origin")
+                                .build()
+                )
+        );
+
+        // When/Then
+        webTestClient.post()
                 .uri("/api/usecase/path")
                 .exchange()
-                .expectStatus().isOk()
+                .expectStatus().isNotFound()
                 .expectHeader().valueEquals("Content-Security-Policy",
                         "default-src 'self'; frame-ancestors 'self'; form-action 'self'")
                 .expectHeader().valueEquals("Strict-Transport-Security", "max-age=31536000;")
